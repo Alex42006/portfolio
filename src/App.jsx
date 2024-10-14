bda031ca93be788001dcc36d07376c03375689b3
Alex42006 <milanesalex941@gmail.com>
2024-10-14 19:18:51 -0400

    React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Title from './components/Title';
+import About from './components/About';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Title/>
+      <About/>
+    </>
+  )
+}
+
+export default App
